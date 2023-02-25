ce23571af92d39264769222eb6a0f30fd418a74e
Mylloon <kennel.anri@tutanota.com>
2023-02-25 19:00:51 +0100

    cleanup useless code

--- Scenes/Screen_Menu.gd
+++ Scenes/Screen_Menu.gd
@@ -2,14 +2,12 @@ extends Control
 
 
 func _on_PlayButton_pressed():
-	print("ici")
 	get_tree().change_scene("res://Scenes/Node2D.tscn")
 
 
 func _on_Help_Button_pressed():
-	print("help")
+	print("help me")
 
 
 func _on_Quit_Button_pressed():
 	get_tree().quit()
-# Replace with function body.
