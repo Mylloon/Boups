21f7746f8ab8be5d8c3c51e67a413d48431a77a2
Mylloon <kennel.anri@tutanota.com>
2023-02-25 17:29:51 +0100

    update villain sprites

--- Characters/Villain.gd
+++ Characters/Villain.gd
@@ -7,33 +7,38 @@ var limit_random
 var rng = RandomNumberGenerator.new()
 onready var button = $"../Button/"
 
+# True if animation should change
+var next = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	timer = 0
 	rng.randomize()
 	limit_random = rng.randi_range(2, 10)
+	me.play("orange")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	timer += delta
-	match me.get_frame():
-		0:
-			# Vert
-			if timer >= limit_random:
-				timer = 0
-				limit_random = rng.randi_range(2, 10)
-				me.set_frame(1)
-		1:
-			# Orange
-			if timer >= .5:
-				timer = 0
-				me.set_frame(2)
-		2:
-			# Rouge
-			if timer >= 2:
-				timer = 0
-				me.set_frame(0)
-		_:
-			print("Not handled frame")
+
+	var animations = ["orange", "red"]
+
+	var max_frame = me.frames.get_frame_count(me.animation) - 1
+	var current_anim_idx = animations.find(me.animation)
+	if current_anim_idx >= animations.size() - 1:
+		# Last animation reached
+		check_next_animframe(me, max_frame, animations[0])
+	else:
+		check_next_animframe(me, max_frame, animations[current_anim_idx + 1])
+
+
+# Jump to next animation at the end of the current one
+func check_next_animframe(me, max_frame, next_animation):
+	if me.frame == 0 and next:
+		me.play(next_animation)
+		next = false
+	if max_frame == me.frame:
+		# Last frame of animation reached
+		next = true
--- Scenes/Button_Node2D.gd
+++ Scenes/Button_Node2D.gd
@@ -3,8 +3,8 @@ extends Button
 onready var villain = $"../Villain/AnimatedSprite"
 
 var etat
-var texture_pressed = preload("res://Assets/skin_assets/button_normal.png")
-var texture_released = preload("res://Assets/skin_assets/button_pressed.png")
+const texture_pressed = preload("res://Assets/skin_assets/button_normal.png")
+const texture_released = preload("res://Assets/skin_assets/button_pressed.png")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,11 +15,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	match villain.get_frame():
-		2:
-			print("non") if etat else print("caché")
-		_:
-			print("oui") if etat else print("caché")
+	if etat:
+		match villain.get_frame():
+			2:
+				print("non")
+			_:
+				print("oui")
+	else:
+		print("caché")
 
 
 func _on_Button_pressed():
